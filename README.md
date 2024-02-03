eb7bffa81aadc47a61539b88a4d4d3a38dd4a160
Dhia M <merzougui.dhia@gmail.com>
2024-02-03 12:17:38 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## TerraVision
+
+A website for a fictional startup called TerraVision, with a mission to create "a clean world in our time".  
+
+Built using React + Tailwind
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+    return (
+        <div className="grid h-screen w-screen place-items-center bg-gradient-to-bl from-white from-60% to-green-100">
+            <h1 className="text-2xl text-green-700">TerraVision</h1>
+        </div>
+    )
+}
+
+export default App

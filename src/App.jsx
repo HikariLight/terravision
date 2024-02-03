21212dae520940fa8a29f1f98a630e56c93c353a
Dhia M <merzougui.dhia@gmail.com>
2024-02-03 12:48:24 +0100

    Added intro section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
+import Intro from "./components/Intro"
 const App = () => {
     return (
-        <div className="grid h-screen w-screen place-items-center bg-gradient-to-bl from-white from-60% to-green-100">
-            <h1 className="text-2xl text-green-700">TerraVision</h1>
-        </div>
+            <Intro />
     )
 }
 

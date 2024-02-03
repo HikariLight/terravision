6b87948bd952e75bde71b1d1073cd45096e92caf
Dhia M <merzougui.dhia@gmail.com>
2024-02-03 15:07:35 +0100

    Added logo to intro page

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,37 +1,53 @@
 import Layout from "./Layout"
+import { Link } from "react-router-dom"
+import Logo from "../assets/logo.png"
 
 const Intro = () => {
     return (
         <Layout>
-            <section
-                className="grid h-full w-full place-items-center bg-gradient-to-bl from-white to-emerald-100 py-12 md:py-24 lg:py-32 xl:py-48"
-                id="intro">
-                <div className="container px-4 md:px-6">
-                    <div className="flex flex-col items-center space-y-4 text-center">
-                        <div className="space-y-2">
-                            <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
-                                Terravision
-                            </h1>
-                            <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
-                                Developing robots to clean and preserve the
-                                planet.
-                            </p>
-                        </div>
-                        <div className="space-x-4">
-                            <a
-                                className="inline-flex h-9 items-center justify-center rounded-md bg-gray-900 px-4 py-2 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
-                                href="#about">
-                                Get Started
-                            </a>
-                            <a
-                                className="inline-flex h-9 items-center justify-center rounded-md border border-gray-200 bg-white px-4 py-2 text-sm font-medium shadow-sm transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300"
-                                href="#features">
-                                Learn more
-                            </a>
+            <div className="grid h-full w-full grid-rows-3">
+                <nav className="flex w-full justify-center space-x-4 pt-4">
+                    <Link to="/" className="hover:opacity-60">
+                        Home
+                    </Link>
+                    <Link to="/contact" className="hover:opacity-60">
+                        Contact
+                    </Link>
+                </nav>
+
+                <section className="grid h-full place-items-center" id="intro">
+                    <div className="container px-4 md:px-6">
+                        <div className="flex flex-col items-center space-y-4 text-center">
+                            <div className="space-y-2">
+                                <img
+                                    className="w-auo mx-auto h-32"
+                                    src={Logo}
+                                    alt=""
+                                />
+                                <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
+                                    Terravision
+                                </h1>
+                                <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
+                                    Developing robots to clean and preserve the
+                                    planet.
+                                </p>
+                            </div>
+                            <div className="space-x-4">
+                                <a
+                                    className="inline-flex h-9 items-center justify-center rounded-md bg-gray-900 px-4 py-2 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
+                                    href="#about">
+                                    Get Started
+                                </a>
+                                <a
+                                    className="inline-flex h-9 items-center justify-center rounded-md border border-gray-200 bg-white px-4 py-2 text-sm font-medium shadow-sm transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300"
+                                    href="#features">
+                                    Learn more
+                                </a>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </section>
+                </section>
+            </div>
         </Layout>
     )
 }
